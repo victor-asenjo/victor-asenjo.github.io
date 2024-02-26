5e4593b6756f55e1d9dd1215d695dc551d3844e5
Víctor Asenjo <victorasenj@gmail.com>
2024-02-26 23:40:01 +0100

    contact and footer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,82 +1,78 @@
 import React from "react";
 import { BsArrowRight } from "react-icons/bs";
 import { RiSendPlaneFill } from "react-icons/ri";
+import { LINKS } from "../../data/links"; // Importa las constantes de los enlaces
+import { FaTelegramPlane } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { RiInstagramFill } from "react-icons/ri";
+import { SiGmail } from "react-icons/si";
 
 const Contact = () => {
   return (
     <div id="contact" className="container m-auto mt-16">
       {/* heading */}
-      <div 
-      // data-aos="fade-up"
-       className="relative mb-5">
+      <div
+        // data-aos="fade-up"
+        className="relative mb-5">
         <h3 className=" text-3xl font-black text-gray-400 sm:text-2xl">
           Contact
         </h3>
         <span className="h-[1.1px] right-0 absolute w-[92%] bg-gray-300 block"></span>
+
       </div>
 
-      {/* card*/}
-      <div className="card-wrapper w-[90%] sm:w-[100%] mx-auto mt-5 flex items-center justify-center sm:flex-col">
-        <div className="left w-[70%] flex-1 flex items-center justify-center sm:flex-col sm:w-full">
-          <div className="flex-3 w-1/2 gap-3 flex items-end justify-end  flex-col sm:w-3/4">
-            <div 
-            data-aos="zoom-in"
-            >
-              <h1 className="text-5xl font-bold sm:text-3xl">You Need</h1>
-              <h3 className="text-xl sm:text-lg">
-                More information about me or you just want to talk...
-              </h3>
-            </div>
-          </div>
-          <div className=" flex p-5 items-center justify-center ">
-            <button
-              
-              data-aos="zoom-in"
-              
-              className= " text-yellow-500 font-extrabold text-3xl p-2 rounded-lg shadow-[0_0_10px_1px_rgba(0,0,0,0.1)] "
-            >
-              <BsArrowRight className=" md:rotate-90" />
-            </button>
-          </div>
-        </div>
-        <div className="right flex-1">
-          <form
-            
-            data-aos="zoom-in"
-            
-            className="flex justify-center items-center flex-col gap-5 w-[70%] md:w-[100%] sm:w-[95%] mx-auto"
-            action="mailto:xyz@gmail.com"
+      <div className=" h-full flex flex-col gap-8 items-center justify-between p-10 sm:p-7">
+        <h2
+          data-aos="zoom-out"
+          className=" font-bold text-5xl sm:text-3xl">Let's Talk</h2>
+        <div className=" flex items-center justify-center gap-8 sm:gap-5">
+          <a
+            data-aos="fade-up" data-aos-duration="1000"
+
+            href={LINKS.email}
+            target="_blank"
+            className="box font-medium flex items-center justify-center flex-col"
           >
-            <input
-              className="px-3 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
-              type="email"
-              placeholder="e.g. example@email.com"
-              name=""
-            />
-            <input
-              className="px-3 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
-              type="text"
-              placeholder="e.g. Víctor Asenjo"
-              name=""
-            />
-            <textarea
-              className="px-3 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
-              rows="4"
-              cols="50"
-              placeholder="Write your message"
-              name=""
-              id=""
-            />
-            <button
-              className="bg-yellow-500 w-full text-white font-semibold  p-2 rounded-lg flex items-center justify-center space-x-1"
-              type="submit"
-            >
-              <span>Send</span>
-              <RiSendPlaneFill/>
-            </button>
-          </form>
+            <SiGmail className=" text-black text-3xl hover:scale-125 cursor-pointer" />
+            <p>Mail</p>
+          </a>
+          <a
+            data-aos="fade-up" data-aos-duration="1000"
+
+            href={LINKS.telegram}
+            target="_blank"
+            className="box font-medium flex items-center justify-center flex-col"
+          >
+            <FaTelegramPlane className=" text-black text-3xl hover:scale-125 cursor-pointer" />
+            <p>Telegram</p>
+          </a>
+          {/*}
+          <a 
+          data-aos="fade-up" data-aos-duration="1200"
+
+            href=""
+            className="box font-medium text-white  flex items-center justify-center flex-col"
+          >
+            <IoLogoWhatsapp className=" text-black text-3xl hover:scale-125 cursor-pointer" />
+            <p>WhatsApp</p>
+          </a>
+  */}
+          <a
+            data-aos="fade-up" data-aos-duration="1400"
+
+            href={LINKS.instagram}
+            target="_blank"
+            className="box font-medium flex items-center justify-center flex-col"
+          >
+            <RiInstagramFill className=" text-black text-3xl hover:scale-125 cursor-pointer" />
+            <p>Instagram</p>
+          </a>
         </div>
       </div>
+
+
+
+
     </div>
   );
 };
