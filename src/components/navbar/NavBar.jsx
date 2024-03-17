f96ed7c72535a2ebe9a466b9b7be9baa684ca7be
Víctor Asenjo <victorasenj@gmail.com>
2024-03-17 22:10:45 +0100

    update

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -110,6 +110,7 @@ const NavBar = ({ toggleDarkMode, darkMode }) => {
               </li>
             ))}
             <a
+              target="_blank" 
               href="https://www.linkedin.com/in/victorasenjo/"
               className="bg-black text-[1rem] text-white px-8 py-2 rounded-lg font-bold hover:text-yellow-400 md:m-5 md:block md:mx-auto md:w-fit lg:px-3"
             >
