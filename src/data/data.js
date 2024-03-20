e5e6724cd170cf21519c581f5ea0de0885567be1
Víctor Asenjo <victorasenj@gmail.com>
2024-03-20 23:18:11 +0100

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -11,11 +11,11 @@ export const data = [
   },
   {
     id: 2,
-    img: 'https://github.com/victor-asenjo/victor-asenjo.github.io/blob/4c5d1192dcda2ada1c84d6db2254ab30358fc6b0/src/assets/images/TelegramBotWork/Diapositiva1.PNG',
+    img: 'https://raw.githubusercontent.com/victor-asenjo/victor-asenjo.github.io/fbc0bb8646c3ae87909d6a98235a1b239c78ddd5/src/assets/images/TelegramBotJpeg/Diapositiva1.JPG',
     title: "Telegram Bot Organizer",
     link: "https://t.me/FCBQbot",
     git: "https://github.com/victor-asenjo",
-  },
+  },  
   {
     id: 3,
     img: 'https://i.postimg.cc/wBjcJcW7/5890625.jpg',
