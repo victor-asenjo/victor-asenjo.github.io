4b663b000d6157430bb5646ec9cc8260e48048a0
Víctor Asenjo <victorasenj@gmail.com>
2024-03-20 23:57:16 +0100

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -3,25 +3,25 @@
 export const data = [
   {
     id: 1,
-    img: 'https://i.postimg.cc/VkRWdHWx/57214s81.jpg',
-    title: "Multimedia Web",
-    desc: "",
-    link: "",
-    git: "https://github.com/victor-asenjo",
-  },
-  {
-    id: 2,
-    img: 'https://raw.githubusercontent.com/victor-asenjo/victor-asenjo.github.io/fbc0bb8646c3ae87909d6a98235a1b239c78ddd5/src/assets/images/TelegramBotJpeg/Diapositiva1.JPG',
+    img: 'https://raw.githubusercontent.com/victor-asenjo/victor-asenjo.github.io/blob/94009e443676bd98d9e7d960a9b996a4add430dd/src/assets/images/Work/Diapositiva1.JPG',
     title: "Telegram Bot Organizer",
     link: "https://t.me/FCBQbot",
     git: "https://github.com/victor-asenjo",
   },  
   {
-    id: 3,
-    img: 'https://i.postimg.cc/wBjcJcW7/5890625.jpg',
-    title: "Music Player",
+    id: 2,
+    img: 'https://raw.githubusercontent.com/victor-asenjo/victor-asenjo.github.io/blob/94009e443676bd98d9e7d960a9b996a4add430dd/src/assets/images/Work/NumberRiddleJpeg.jpg',
+    title: "Number Riddle Game",
     desc: "",
-    link: "",
+    link: "https://play.google.com/",
+    git: "https://github.com/victor-asenjo",
+  },
+  {
+    id: 3,
+    img: 'https://raw.githubusercontent.com/victor-asenjo/victor-asenjo.github.io/blob/94009e443676bd98d9e7d960a9b996a4add430dd/src/assets/images/Work/PasswordGeneratorJpeg.jpg',
+    title: "Password Generator App",
+    desc: "",
+    link: "https://play.google.com/",
     git: "https://github.com/victor-asenjo",
   }
   
